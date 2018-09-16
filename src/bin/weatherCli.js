4c76848bf5d039aa64dd41c1820af9a6213aa3be
mimikria96 <chemiakina7493@mail.ru>
2018-09-16 02:37:44 +0200

    cli

--- src/bin/weatherCli.js
+++ src/bin/weatherCli.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
-import programCli from '../programm';
 
+import program from 'commander';
+import getWeather from '../weather';
 
-programCli();
+console.log('tst');
+export default program
+  .version('0.1.0')
+  .description('Return weather by ip')
+  .arguments('<firstConfig>')
+  .option('-f, --format [type]', 'Parse format', 'default')
+  .action(async (firstConfig, type) => console.log(getWeather(firstConfig, type.format)))
+  .parse(process.argv);
